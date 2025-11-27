12901606c6898d40fdd9b22a53011a9f0ebf593b
yabamiah <211063265@aluno.unb.br>
2025-11-27 19:53:30 -0300

    [Refact] Extrair Método, <Campeonato> / <gerar_rodadas>
    Co-authored-by: Luis Miranda <luiseduardocarneiro6@gmail.com>

--- organizadorCampeonatoBrasileiro/campeonato.py
+++ organizadorCampeonatoBrasileiro/campeonato.py
@@ -1,8 +1,6 @@
-# Classe para definir os 20 times do campeonato brasileiro, gerar rodadas e definir a classificação final do campeonato.
 class Campeonato:
     def __init__(self, equipes: list[str]):
         """Inicializa o campeonato."""
-
         self.equipes = equipes
         self.num_times = len(equipes)
         self.jogos = []
@@ -19,40 +17,52 @@ class Campeonato:
         - Cada time jogue 19 partidas como mandante e 19 como visitante
         - Total de 38 rodadas com 10 partidas cada
         """
-
         self.jogos = []
-        # Cria uma cópia da lista de equipes para não modificar a original
         equipes_trabalho = self.equipes.copy()
+
+        self.gerar_primeiro_turno(equipes_trabalho)
+        self.gerar_retornos()
+
+    def gerar_primeiro_turno(self, equipes_trabalho):
+        """Gera o primeiro turno do campeonato (19 rodadas)."""
         metade_campeonato = self.num_times // 2
 
-        # Primeiro Turno (19 rodadas)
         for rodada_num in range(self.num_times - 1):
-            jogos_rodada = []
-            
-            for i in range(metade_campeonato):
-                time_a = equipes_trabalho[i]
-                time_b = equipes_trabalho[self.num_times - 1 - i]
-
-                # Alterna mandos por rodada para equilibrar casa/fora
-                if rodada_num % 2 == 0:
-                    jogos_rodada.append((time_a, time_b))
-                else:
-                    jogos_rodada.append((time_b, time_a))
-            
+            jogos_rodada = self.gerar_jogos_da_rodada(equipes_trabalho, rodada_num)
             self.jogos.append(jogos_rodada)
+            equipes_trabalho = self.rotacionar_times(equipes_trabalho)
 
-            # Rotaciona os times mantendo o primeiro fixo (algoritmo Round-Robin)
-            equipes_trabalho = [equipes_trabalho[0], equipes_trabalho[-1]] + equipes_trabalho[1:-1]
+    def gerar_jogos_da_rodada(self, equipes_trabalho, rodada_num):
+        """Gera os jogos de uma rodada específica, alternando mandos de campo."""
+        jogos_rodada = []
+        metade_campeonato = self.num_times // 2
+        
+        for i in range(metade_campeonato):
+            time_a = equipes_trabalho[i]
+            time_b = equipes_trabalho[self.num_times - 1 - i]
 
-        # Returno (19 rodadas) - inverte todos os mandos do turno
+            # Alterna mandos por rodada para equilibrar casa/fora
+            if rodada_num % 2 == 0:
+                jogos_rodada.append((time_a, time_b))
+            else:
+                jogos_rodada.append((time_b, time_a))
+                
+        return jogos_rodada
+
+    def rotacionar_times(self, equipes_trabalho):
+        """Rotaciona os times mantendo o primeiro fixo (algoritmo Round-Robin)."""
+        return [equipes_trabalho[0], equipes_trabalho[-1]] + equipes_trabalho[1:-1]
+
+    def gerar_retornos(self):
+        """Gera o returno do campeonato (19 rodadas), invertendo os mandos de campo."""
         primeiro_turno = list(self.jogos)
+        
         for rodada in primeiro_turno:
             rodada_returno = [(visitante, mandante) for mandante, visitante in rodada]
             self.jogos.append(rodada_returno)
-        
+
     def exibir_jogos(self):
         """Imprime o calendário de jogos gerado."""
-
         for i, rodada in enumerate(self.jogos):
             print(f"--- Rodada {i + 1} ---")
             for mandante, visitante in rodada:
@@ -65,4 +75,4 @@ times_brasileirao_serie_A = ["Flamengo", "Palmeiras", "São Paulo", "Corinthians
     
 brasileirao_serie_A = Campeonato(times_brasileirao_serie_A)
 brasileirao_serie_A.gerar_rodadas()
-print(brasileirao_serie_A.exibir_jogos())
+brasileirao_serie_A.exibir_jogos()
